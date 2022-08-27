b606519d5be1e6fb8a5534b7c2b8e02c8c986da9
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-08-27 15:01:23 -0300

    First settings

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,13 +1,13 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://fyuji.github.io/cleanfolio',
+  title: 'Portfolio',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Fabio Yuji Soyama',
+  role: 'Software Developer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
