3379032dd49fc98d4a7637121c96ded11ba4ef27
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-08-29 11:35:21 -0300

    Some fixes in projects and experiences

--- src/portfolio.js
+++ src/portfolio.js
@@ -54,51 +54,53 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Web Portfolio',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'This personal web portfolio. Developed as my first contact with React (Front-end) and Python (Back-end)',
+    stack: ['React', 'HTML5', 'CSS3', 'Python'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Comming soon...',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      '- - - - - - - - - - - - - - -',
+    stack: ['-', '-', '-'],
+    sourceCode: '',
+    livePreview: '',
   },
   {
-    name: 'Project 3',
+    name: 'Comming soon...',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      '- - - - - - - - - - - - - - -',
+    stack: ['-', '-', '-'],
+    sourceCode: '',
+    livePreview: '',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'SQL Server',
+  '.NET',
+  'Flutter',
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Java',
+  'C',
+  'C++',
+  'Python',
   'Git',
+  'DevOps',
   'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: '',
+  email: 'asdasd',
 }
 
 export { header, about, projects, skills, contact, formations, experiences }
